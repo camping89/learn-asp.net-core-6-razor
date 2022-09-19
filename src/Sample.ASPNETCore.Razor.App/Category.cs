665237cb203c9667749bb547a48fa727b91b294e
vu.nguyen.sf <camping89@gmail.com>
2022-09-20 00:28:03 +0700

    * wip db context

--- src/Sample.ASPNETCore.Razor.App/Category.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sample.ASPNETCore.Razor.App;
-
-public class Category
-{
-    [Key]
-    public int Id { get; set; }
-    public string Name{ get; set; }
-}
---
+++ src/Sample.ASPNETCore.Razor.App/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sample.ASPNETCore.Razor.App.Model;
+
+namespace Sample.ASPNETCore.Razor.App.Data;
+
+public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext()
+    {
+        
+    }
+
+    public DbSet<Category> Categories { get; set; }
+}
